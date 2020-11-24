141ada7e4c8d0fd4b12031cb04a15202759a8d6d
Mahima <mahimasachdeva223@gmail.com>
2020-11-24 16:28:15 +0530

    React Components part 1

--- React/confusion/src/components/MenuComponent.js
+++ React/confusion/src/components/MenuComponent.js
@@ -1,37 +1,77 @@
 import React, { Component } from 'react';
-
+import { Media } from 'reactstrap';
 
 class Menu extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            dishes: [
+                {
+                  id: 0,
+                  name:'Uthappizza',
+                  image: 'assets/images/uthappizza.png',
+                  category: 'mains',
+                  label:'Hot',
+                  price:'4.99',
+                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
+               {
+                  id: 1,
+                  name:'Zucchipakoda',
+                  image: 'assets/images/zucchipakoda.png',
+                  category: 'appetizer',
+                  label:'',
+                  price:'1.99',
+                  description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
+               {
+                  id: 2,
+                  name:'Vadonut',
+                  image: 'assets/images/vadonut.png',
+                  category: 'appetizer',
+                  label:'New',
+                  price:'1.99',
+                  description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
+               {
+                  id: 3,
+                  name:'ElaiCheese Cake',
+                  image: 'assets/images/elaicheesecake.png',
+                  category: 'dessert',
+                  label:'',
+                  price:'2.99',
+                  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
+            ],
+        };
     }
 
     render() {
-        return (
 
+        const menu = this.state.dishes.map((dish) => {
+            return (
+              <div key={dish.id} className="col-12 mt-5">
+                <Media tag="li">
+                  <Media left middle>
+                      <Media object src={dish.image} alt={dish.name} />
+                  </Media>
+                  <Media body className="ml-5">
+                    <Media heading>{dish.name}</Media>
+                    <p>{dish.description}</p>
+                  </Media>
+                </Media>
+              </div>
+            );
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <Media list>
+                        {menu}
+                    </Media>
+                </div>
+            </div>
         );
     }
 
 }
 
 export default Menu;
-
-/* To add in the new component in here, let me first import React and then also import Component from React. 
-    Because that is what will allow me to create a React Component. 
-    And once we do this, let's create our new component here as a class component and so I will type class Menu extends Component. 
-    As you can see, this is how you would add in a new component to React Application and the name of this new component is Menu. 
-    And within this menu component, I'm going to first define the constructor for this component. 
-    So, we'll define the constructor and this constructor gets a parameter called props. 
-    With this constructor props, I need to supply this props to my super class. 
-    So, I say super props here, and this is required whenever you define a class component. 
-    Furthermore, any component in React, a class component should implement this method called render() which will turn the corresponding view for this component. 
-    So, inside this render method, I need to return the view for this component. 
-    So, I'm going to create this return value here and you can see the red squiggly line, telling me that this is empty at the moment so I need to fill that in appropriately. 
-    Also when you create the component, don't forget to export that component from this file, because we would need to import this component wherever we want to make use of it within our application. 
-    So, there you go. We have completed the basic structure for a component. A class component would be defined like this. 
-    So, you first extend the component, you define the constructor for it, and then also implement a function called render(), 
-    inside this component which will return what needs to be displayed on the UI by this component. 
-    And don't forget to export the component from this file. That's it. 
-    So, any time you need to create a new component, this is how you would go about creating that new component. 
-*/
