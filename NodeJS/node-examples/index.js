a2fec3f7a8576beb6b01a1d5623ae658fee9c498
Mahima <mahimasachdeva223@gmail.com>
2021-01-14 19:39:10 +0530

    simple node module

--- NodeJS/node-examples/index.js
+++ NodeJS/node-examples/index.js
@@ -1,9 +1,11 @@
 //npm init creates package.json file.
 
-var rect = {            //this is a js object
-	perimeter: (x, y) => (2*(x+y)),
-	area: (x, y) => (x*y)
-};
+var rect = require('./rectangle');
+
+// var rect = {            //this is a js object
+// 	perimeter: (x, y) => (2*(x+y)),
+// 	area: (x, y) => (x*y)
+// };
 
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
---
+++ NodeJS/node-examples/rectangle.js
@@ -0,0 +1,4 @@
+exports.perimeter =  (x, y) => (2*(x+y));
+exports.area = (x, y) => (x*y);
+
+//this is a simple node module that is exporting 2 values-area and perimeter
